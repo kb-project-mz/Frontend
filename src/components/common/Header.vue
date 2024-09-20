3bca30f2e3b76f29a4f751e99b5507c6c235f969
woo427 <annejwkim00@naver.com>
2024-09-20 15:03:06 +0900

    [SCRUM-147, SCRUM-207] Feat: 로그인 전 홈 화면 구현
    1. 글자 색상, 글자 사이즈 위치, 버튼 사이즈 수정사항 변경완료
    2. 로그 클릭 시 홈 화면으로 이동

---
+++ src/components/common/Header.vue
@@ -0,0 +1,17 @@
+<script setup></script>
+
+<template>
+  <nav class="bg-white shadow-lg p-4 rounded-xl max-w-9xl mt-4 mx-auto">
+    <div class="max-w-7.5xl mx-auto flex justify-between items-center">
+      <router-link to="/" class="flex items-center">
+        <img src="/src/assets/로고.png" alt="Logo" class="h-8 w-auto" />
+      </router-link>
+
+      <div class="flex space-x-4">
+        <a href="#" class="gray">로그인</a>
+        <span class="gray">|</span>
+        <a href="#" class="gray">회원가입</a>
+      </div>
+    </div>
+  </nav>
+</template>
