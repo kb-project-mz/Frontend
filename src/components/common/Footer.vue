c26a473b29d205528a57a69e78fdaf13c811068f
다영 <86220363+imdayoung@users.noreply.github.com>
2024-09-25 00:59:11 +0900

    [SCRUM-255] Design: Header, Footer 디자인 수정 및 로그인 여부 적용

--- src/components/common/Footer.vue
+++ src/components/common/Footer.vue
@@ -3,12 +3,35 @@
 </script>
 
 <template>
-  <div class="footer mt-20 h-80">
-    FOOTER
+  <div class="footer flex flex-col justify-center items-center mt-16 min-h-72 text-gray-500 text-sm">
+    <div class="flex space-x-5 mb-5">
+      <img src="/src/assets/notion.png" />
+      <img src="/src/assets/slack.png" />
+      <img src="/src/assets/github.png" />
+    </div>
+    
+    <div>KB IT's Your Life 5기 깨비핑</div>
+    
+    <div class="flex items-center space-x-1">
+      <font-awesome-icon :icon="['fas', 'phone']" style="color: #71717A;" />
+      <div>02-2205-0108</div>
+    </div>
+    <div>서울특별시 광진구 능동로 195-16, 5층</div>
+    
+
+    <div class="flex space-x-3 mt-5">
+      <div>이용 약관</div>
+      <div>|</div>
+      <div>개인 정보 처리 방침</div>
+    </div>
   </div>
 </template>
 
 <style scoped>
+img {
+  width: 2rem;
+  height: 2rem;
+}
 .footer {
   background-color: #F5F6F7;
 }
--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -1,35 +1,28 @@
 <script setup>
-import { ref, onMounted } from 'vue';
+import { ref, computed } from 'vue';
+import { useAuthStore } from '@/stores/auth';
 
-const memberName = ref(null);
-
-onMounted(() => {
-  memberName.value = localStorage.getItem("memberName");
-});
+const authStore = useAuthStore();
+const id = computed(() => authStore.member.id);
+const memberName = computed(() => authStore.member.memberName);
 
 const logout = () => {
-  localStorage.removeItem("memberId");
-  localStorage.removeItem("memberName");
-  localStorage.removeItem("accessToken");
-  localStorage.removeItem("refreshToken");
-  localStorage.removeItem("auth");
-
-  memberName.value = null;
+  authStore.logout();
 };
 </script>
 
 <template>
-  <nav class="bg-white shadow-lg p-4 rounded-xl max-w-9xl mt-4 mx-auto">
+  <nav class="bg-white shadow-lg p-4 rounded-xl max-w-9xl mt-4 mx-auto mb-16">
     <div class="max-w-7.5xl mx-auto flex justify-between items-center">
       <router-link to="/" class="flex items-center">
-        <img src="/src/assets/로고.png" alt="Logo" class="h-8 w-auto" />
+        <img src="/src/assets/logo.png" alt="Logo" class="h-8 w-auto" />
       </router-link>
 
     <div v-if="id">
       <router-link to="/mypage">
         {{ memberName }}님
       </router-link>
-      <button @click="logout" class="gray">로그아웃</button>
+      <button @click="logout" class="bg-navy ml-2 py-1.5 px-2.5 text-white rounded-md text-xs">로그아웃</button>
     </div>
 
     <div v-else>
@@ -46,3 +39,9 @@ const logout = () => {
     </div>
   </nav>
 </template>
+
+<style scoped>
+.bg-navy {
+  background-color: #0B1573;
+}
+</style>
