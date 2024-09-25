05d5033dc3a68a874c73ae4c144445bff2dfc294
2da00 <86220363+imdayoung@users.noreply.github.com>
2024-09-25 17:08:21 +0900

    Refactor: account-history.js 탭 사이즈 수정

--- src/stores/account-history.js
+++ src/stores/account-history.js
@@ -2,38 +2,38 @@ import { defineStore } from "pinia";
 import apiInstance from "./axios-instance";
 
 export const useAccountHistoryStore = defineStore("accountHistory", {
-    state: () => ({
-        accountHistory: [],
-        accountHistoryThisMonth: [],
-    }),
+  state: () => ({
+    accountHistory: [],
+    accountHistoryThisMonth: [],
+  }),
 
-    actions: {
-        async getAccountHistoryList(memberId) {
-            try {
-                const res = await apiInstance.get(`/history/account/${memberId}`, {
-                    headers: {
-                        Authorization:
-                            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJkbGVrZHVkMDEwMiIsInJvbGUiOiJST0xFX1VTRVIiLCJpYXQiOjE3MjcwOTMxMDQsImV4cCI6MTcyNzQzODcwNH0.CDpk_rBMERwApocgtppe8EtnuQkbxRpjLAJK2A7wBBo",
-                    },
-                });
-                this.accountHistory = res.data.data;
+  actions: {
+    async getAccountHistoryList(memberId) {
+      try {
+        const res = await apiInstance.get(`/history/account/${memberId}`, {
+          headers: {
+            Authorization:
+              "Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJkbGVrZHVkMDEwMiIsInJvbGUiOiJST0xFX1VTRVIiLCJpYXQiOjE3MjcwOTMxMDQsImV4cCI6MTcyNzQzODcwNH0.CDpk_rBMERwApocgtppe8EtnuQkbxRpjLAJK2A7wBBo",
+          },
+        });
+        this.accountHistory = res.data.data;
 
-                const now = new Date();
-                const thisYear = now.getFullYear();
-                const thisMonth = now.getMonth() + 1;
+        const now = new Date();
+        const thisYear = now.getFullYear();
+        const thisMonth = now.getMonth() + 1;
 
-                this.accountHistoryThisMonth = this.accountHistory.filter((history) => {
-                    const accountDate = new Date(history.accountDate);
-                    const accountYear = accountDate.getFullYear();
-                    const accountMonth = accountDate.getMonth() + 1;
+        this.accountHistoryThisMonth = this.accountHistory.filter((history) => {
+          const accountDate = new Date(history.accountDate);
+          const accountYear = accountDate.getFullYear();
+          const accountMonth = accountDate.getMonth() + 1;
 
-                    return accountYear === thisYear && accountMonth == thisMonth;
-                });
+          return accountYear === thisYear && accountMonth == thisMonth;
+        });
 
-                console.log(this.accountHistory);
-            } catch (err) {
-                console.error(err);
-            }
-        },
+        console.log(this.accountHistory);
+      } catch (err) {
+        console.error(err);
+      }
     },
+  },
 });
