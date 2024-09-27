25d0bf68561dbe8ba6b98b4f9a45c08fe5d6bb5b
DYA <sally7410@naver.com>
2024-09-27 17:43:36 +0900

    [SCRUM-259] Feat: 월별 총 지출 내역 그래프 구현
    월별 총 지출 내역 그래프 구현
    지출 내역 스크롤 생성 -> 페이지네이션 기능 추가 예정
    ConsumptionPage 전반적 배치 수정

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -1,97 +1,95 @@
-import { defineStore } from 'pinia';
-import apiInstance from './axios-instance';
+import { defineStore } from "pinia";
+import apiInstance from "./axios-instance";
 
-export const useAuthStore = defineStore('auth', {
-  state: () => ({
-    member: {
-			id: localStorage.getItem('id') || null,
-      memberName: localStorage.getItem('memberName') || null,
-      memberId: localStorage.getItem('memberId') || null
-    }
-  }),
-  actions: {
-    async login(memberId, password) {
-			try {
-				console.log('로그인 시도:', memberId);
-				const response = await apiInstance.post('/member/login', {
-					memberId: memberId,
-					password: password
-				});
-				console.log(response.data);
-		
-				const loginData = response.data.data;
-		
-				if (!response.data.success) {
-					console.error(response.data.error.message);
-					return response.data.error.message;
-				}
+export const useAuthStore = defineStore("auth", {
+    state: () => ({
+        member: {
+            id: localStorage.getItem("id") || null,
+            memberName: localStorage.getItem("memberName") || null,
+            memberId: localStorage.getItem("memberId") || null,
+        },
+    }),
+    actions: {
+        async login(memberId, password) {
+            try {
+                console.log("로그인 시도:", memberId);
+                const response = await apiInstance.post("/member/login", {
+                    memberId: memberId,
+                    password: password,
+                });
+                console.log(response.data);
 
-        this.member.id = loginData.id;
-        this.member.memberId = loginData.memberId;
-        this.member.memberName = loginData.memberName;
+                const loginData = response.data.data;
 
-        localStorage.setItem('id', loginData.id);
-				localStorage.setItem('memberId', loginData.memberId);		
-        localStorage.setItem('memberName', loginData.memberName);
-				localStorage.setItem('accessToken', 'Bearer ' + loginData.accessToken);
-				localStorage.setItem('refreshToken', 'Bearer ' + loginData.refreshToken);
-				localStorage.setItem('auth', JSON.stringify(loginData));				
-		
-				console.log('로그인 응답 데이터:', loginData);
-				return loginData;
-			} catch (error) {
-				console.error('로그인 중 오류:', error);
-				throw error;
-			}
-		},
-		
-    async create(member) {
-      try {
-        const response = await apiInstance.post('/member/join', member);
-        return response.data.data;
-      } catch (error) {
-        console.error('회원가입 중 오류:', error.response ? error.response.data : error.message);
-        throw error;
-      }
+                if (!response.data.success) {
+                    console.error(response.data.error.message);
+                    return response.data.error.message;
+                }
+
+                this.member.id = loginData.id;
+                this.member.memberId = loginData.memberId;
+                this.member.memberName = loginData.memberName;
+
+                localStorage.setItem("id", loginData.id);
+                localStorage.setItem("memberId", loginData.memberId);
+                localStorage.setItem("memberName", loginData.memberName);
+                localStorage.setItem("accessToken", "Bearer " + loginData.accessToken);
+                localStorage.setItem("refreshToken", "Bearer " + loginData.refreshToken);
+                localStorage.setItem("auth", JSON.stringify(loginData));
+
+                console.log("로그인 응답 데이터:", loginData);
+                return loginData;
+            } catch (error) {
+                console.error("로그인 중 오류:", error);
+                throw error;
+            }
+        },
+
+        async create(member) {
+            try {
+                const response = await apiInstance.post("/member/join", member);
+                return response.data.data;
+            } catch (error) {
+                console.error("회원가입 중 오류:", error.response ? error.response.data : error.message);
+                throw error;
+            }
+        },
+        async checkMemberId(memberId) {
+            try {
+                const response = await apiInstance.get(`/member/check-memberId/${memberId}`);
+                console.log("API 응답:", response);
+                return response.data.data;
+            } catch (error) {
+                console.error("ID 중복 확인 중 오류:", error);
+                throw error;
+            }
+        },
+        async checkEmail(email) {
+            try {
+                const response = await apiInstance.get(`/member/check-email?email=${encodeURIComponent(email)}`);
+                return response.data.data;
+            } catch (error) {
+                console.error("이메일 체크 중 오류:", error);
+                throw error;
+            }
+        },
+        logout() {
+            this.member.id = null;
+            this.member.memberName = null;
+            this.member.memberId = null;
+            localStorage.clear();
+        },
     },
-    async checkMemberId(memberId) {
-      try {
-        const response = await apiInstance.get(`/member/check-memberId/${memberId}`);
-        console.log("API 응답:", response);
-        return response.data.data;
-      } catch (error) {
-        console.error('ID 중복 확인 중 오류:', error);
-        throw error;
-      }
+    // 로그인 상태 유지
+    loadAuthState() {
+        const authData = localStorage.getItem("auth");
+        if (authData) {
+            this.member = JSON.parse(authData);
+        }
     },
-    async checkEmail(email) {
-      try {
-        const response = await apiInstance.get(`/member/check-email?email=${encodeURIComponent(email)}`);
-        return response.data.data;
-      } catch (error) {
-        console.error('이메일 체크 중 오류:', error);
-        throw error;
-      }
+
+    isLogin() {
+        const authData = localStorage.getItem("auth");
+        return !!authData;
     },
-    logout() {
-      this.member.id = null;
-      this.member.memberName = null;
-      this.member.memberId = null;
-      localStorage.clear();
-    }
-  },
-	// 로그인 상태 유지
-	loadAuthState() {
-		const authData = localStorage.getItem('auth');
-		if (authData) {
-			this.member = JSON.parse(authData);
-		}
-	},
-
-	isLogin() {
-		const authData = localStorage.getItem('auth');
-		return !!authData;
-	},
-
-
 });
