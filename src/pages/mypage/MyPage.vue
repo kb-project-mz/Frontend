dd2a6f4dc311468fdbb96ab8c8f16bf93ed22f72
user.name <95dbswn@hanmail.net>
2024-10-01 17:45:58 +0900

    [SCRUM-180] Feat: 프론트 이미지 업로드 기능 구현중
    - 파일 업로드만 가능

--- src/pages/mypage/MyPage.vue
+++ src/pages/mypage/MyPage.vue
@@ -1,5 +1,33 @@
 <script setup>
+import {ref} from 'vue';
+import axios from 'axios';
+import apiInstance from '@/util/axios-instance';
 
+const fileInput = ref(null);
+
+const uploadFile = async () => {
+  const file = fileInput.value.files[0];
+  if(!file) {
+    alert('파일을 선택해주세요.');
+    return;
+  }
+
+  const formData = new FormData();
+  formData.append('profileImage', file);
+
+  try {
+    const response = await apiInstance.post(`/member/info`, formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    });
+    console.log('파일 업로드 성공');
+    alert('파일 업로드 성공');
+  } catch (error) {
+    console.error('파일 업로드 실패:', error);
+    alert('파일 업로드 실패!');
+  }
+}
 </script>
 
 <template>
@@ -7,6 +35,10 @@
     <div class="inline-block p-3 bg-navy text-white rounded-lg">
       <router-link to="/mypage/asset">연결된 자산 보기</router-link>
     </div>
+    <form @submit.prevent="uploadFile" enctype="multipart/form-data">
+      <li>파일 <input type="file" name="file" ref="fileInput"> </li>
+      <button type="submit" class="p-2 bg-blue-500 text-white rounded-lg"></button>
+    </form>
   </div>
 </template>
 
