09054cc083c34e61333d828a0eafc1a953061d04
woo427 <annejwkim00@naver.com>
2024-10-02 17:23:46 +0900

    [SCRUM-211] Feat: 챌린지 top3 그래프 초기 구현
    그래프는 구현했지만 진행상황이 반영이 되지 않았음.

--- src/components/home/Challenge.vue
+++ src/components/home/Challenge.vue
@@ -1,9 +1,9 @@
 <script setup>
+import ProgressBar from "@/components/challenge/ProgressBar.vue";
 import { useChallengeTop3Store } from "@/stores/challengeTop3";
 import { ref, onMounted } from "vue";
 
 const challengeTop3Store = useChallengeTop3Store();
-
 const challengeTop3ByMember = ref([]);
 const memberIdx = localStorage.getItem("memberIdx");
 
@@ -14,6 +14,7 @@ const fetchChallengeTop3 = async (memberIdx) => {
 onMounted(async () => {
   await fetchChallengeTop3(memberIdx);
   challengeTop3ByMember.value = challengeTop3Store.ChallengeTop3List;
+  console.log("데이터를 불러와보자 :", challengeTop3ByMember);
 });
 </script>
 
@@ -22,12 +23,21 @@ onMounted(async () => {
     -----------------------------------------------
     <h1><챌린지 탑3></h1>
   </div>
-  <div>
+  <!-- <div>
     <ul>
-      <!-- 배열을 순회하면서 challengeName 출력 -->
       <li v-for="(challenge, index) in challengeTop3ByMember" :key="index">
         {{ challenge.challengeName }}
       </li>
     </ul>
+  </div> -->
+  <!-- <ProgressBar class="mt-8" v-if="challengeTop3ByMember" :limit="challengeTop3ByMember.challengeLimit" :completed="challengeTop3ByMember.cardHistoryCount" /> -->
+  <div v-for="challenge in challengeTop3ByMember" :key="challenge.challengeIdx">
+    <h1>{{ challenge.challengeName }}</h1>
+    <ProgressBar
+      class="mt-8 w-3/4"
+      v-if="challenge"
+      :limit="challenge.challengeLimit"
+      :completed="challenge.cardHistoryCount"
+    />
   </div>
 </template>
