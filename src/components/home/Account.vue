59f006da2659b16c479fbc0210cb3d0100ed62b9
woo427 <annejwkim00@naver.com>
2024-10-03 14:05:43 +0900

    Feat: 챌린지 top3 그래프, 챌린지 바로가기 구현
    1. AccountBook.vue 컴포넌트를 DB에 맞게 Account.vue로 이름 변경
    2. 챌린지 top3 그래프, 챌린지 바로가기 구현

--- src/components/home/Account.vue
+++ src/components/home/Account.vue
@@ -1,16 +1,16 @@
 <script setup>
-import { ref, onMounted } from "vue";
-import { io } from "socket.io-client";
-import { useBalanceStore } from "@/stores/balance";
+import { ref, onMounted } from 'vue';
+import { io } from 'socket.io-client';
+import { useBalanceStore } from '@/stores/balance';
 
 // 서버에서 데이터를 수신할 배열
 const balanceByMember = ref([]);
 
 const balanceStore = useBalanceStore();
-const memberIdx = localStorage.getItem("memberIdx");
+const memberIdx = localStorage.getItem('memberIdx');
 
 // Socket.IO 클라이언트 연결
-const socket = io("http://localhost:3000");
+const socket = io('http://localhost:3000');
 
 const fetchBalance = async (memberIdx) => {
   await balanceStore.getTotalBalance(memberIdx);
@@ -21,18 +21,18 @@ onMounted(async () => {
   balanceByMember.value = balanceStore.TotalBalanceList; // 업데이트된 데이터 반영
 
   // 연결 성공 시 출력
-  socket.on("connect", () => {
-    console.log("Socket.IO connected!");
+  socket.on('connect', () => {
+    console.log('Socket.IO connected!');
   });
 
   // 연결 오류 시 출력
-  socket.on("connect_error", (err) => {
-    console.error("Connection error:", err);
+  socket.on('connect_error', (err) => {
+    console.error('Connection error:', err);
   });
 
   // 서버에서 balance 업데이트 이벤트 수신
-  socket.on("balanceUpdate", (data) => {
-    console.log("Balance updated: ", data);
+  socket.on('balanceUpdate', (data) => {
+    console.log('Balance updated: ', data);
     balanceByMember.value = data; // 업데이트된 데이터 반영
   });
 });
@@ -40,6 +40,6 @@ onMounted(async () => {
 
 <template>
   <div v-for="(item, index) in balanceByMember" :key="index">
-    <div>{{ item.accountName }} - {{ item.balance }}</div>
+    {{ item.accountName }} - {{ item.balance }}
   </div>
 </template>
--- src/components/home/Challenge.vue
+++ src/components/home/Challenge.vue
@@ -1,11 +1,11 @@
 <script setup>
-import ProgressBar from "@/components/challenge/ProgressBar.vue";
-import { useChallengeTop3Store } from "@/stores/challengeTop3";
-import { ref, onMounted } from "vue";
+import ProgressBar from '@/components/challenge/ProgressBar.vue';
+import { useChallengeTop3Store } from '@/stores/challengeTop3';
+import { ref, onMounted } from 'vue';
 
 const challengeTop3Store = useChallengeTop3Store();
 const challengeTop3ByMember = ref([]);
-const memberIdx = localStorage.getItem("memberIdx");
+const memberIdx = localStorage.getItem('memberIdx');
 
 const fetchChallengeTop3 = async (memberIdx) => {
   await challengeTop3Store.getChallengeTop3(memberIdx);
@@ -14,30 +14,26 @@ const fetchChallengeTop3 = async (memberIdx) => {
 onMounted(async () => {
   await fetchChallengeTop3(memberIdx);
   challengeTop3ByMember.value = challengeTop3Store.ChallengeTop3List;
-  console.log("데이터를 불러와보자 :", challengeTop3ByMember);
 });
 </script>
 
 <template>
-  <div>
-    -----------------------------------------------
-    <h1><챌린지 탑3></h1>
-  </div>
-  <!-- <div>
-    <ul>
-      <li v-for="(challenge, index) in challengeTop3ByMember" :key="index">
-        {{ challenge.challengeName }}
-      </li>
-    </ul>
-  </div> -->
-  <!-- <ProgressBar class="mt-8" v-if="challengeTop3ByMember" :limit="challengeTop3ByMember.challengeLimit" :completed="challengeTop3ByMember.cardHistoryCount" /> -->
-  <div v-for="challenge in challengeTop3ByMember" :key="challenge.challengeIdx">
-    <h1>{{ challenge.challengeName }}</h1>
-    <ProgressBar
-      class="mt-8 w-3/4"
-      v-if="challenge"
-      :limit="challenge.challengeLimit"
-      :completed="challenge.cardHistoryCount"
-    />
+  <h1><챌린지 탑3></h1>
+  <RouterLink to="/challenge"> 챌린지 보러 가기! </RouterLink>
+  <div v-if="challengeTop3ByMember.length === 0">또래 챌린지 보여줘야함.</div>
+  <div v-else>
+    <div
+      v-for="challenge in challengeTop3ByMember"
+      :key="challenge.challengeIdx"
+      class="w-full"
+    >
+      <h1>{{ challenge.challengeName }}</h1>
+      <ProgressBar
+        class="mt-8 w-3/4"
+        v-if="challenge"
+        :limit="challenge.challengeLimit"
+        :completed="challenge.cardHistoryCount"
+      />
+    </div>
   </div>
 </template>
