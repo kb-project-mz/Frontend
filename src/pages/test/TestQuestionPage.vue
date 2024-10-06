139edfbf2ab9bfe354d661bc1328d3b2d372db56
Lee Doha <79359308+leedoha11@users.noreply.github.com>
2024-10-06 21:47:18 +0900

    [SCRUM-301] Feat: 백엔드 api로 불러온 데이터를 프론트에 출력
    - 프론트에 출력해서 넘어가는 것 까지는 됩니다.

--- src/pages/test/TestQuestionPage.vue
+++ src/pages/test/TestQuestionPage.vue
@@ -1,14 +1,36 @@
 <script setup>
 import { useRouter, useRoute } from "vue-router";
-import { computed, ref } from "vue";
+import { computed, ref, onMounted, watch } from "vue";
+import { useTestStore } from "@/stores/test";
 
 const router = useRouter();
 const route = useRoute();
+const testStore = useTestStore();
+
+const questions = ref([]);
+const options = ref([]);
 
 const questionId = computed(() => parseInt(route.params.number));
+const currentQuestion = computed(() => testStore.questions[questionId.value - 1]); // 현재 질문 가져오기
+const currentOptions = computed(() => testStore.getOptionsByQuestionIdx(questionId.value)); // 현재 질문에 대한 옵션 가져오기
+const answers = computed(() => currentOptions.value);
+
+const fetchData = async () => {
+    questions.value = await testStore.fetchQuestions(); 
+    options.value = await testStore.fetchOptions(questionId.value); 
+    console.log('011111111111110', options.value);
+}
+
+onMounted(() => {
+    fetchData(); 
+})
+
+watch(questionId, () => {
+    fetchData(); 
+});
 
 const nextQuestion = () => {
-    if (questionId.value < 12) {
+    if (questionId.value < testStore.questions.length) {
         router.push({ name: "testQuestion", params: { number: questionId.value + 1 } });
     } else {
         router.push({ name: "testResult" });
@@ -16,10 +38,6 @@ const nextQuestion = () => {
 };
 
 // 서버에서 데이터를 받아와 버튼 내용 설정해야합니다 !!!
-const answers = ref([
-    { id: 1, text: "이건 사야 해! 할인은 다시 오지 않는 기회! 바로 산다 😎" },
-    { id: 2, text: "안 사면 100% 할인.. 예전에 입던 소비만이 참는다 🤔" },
-]);
 </script>
 
 <template>
@@ -27,16 +45,17 @@ const answers = ref([
         <div class="text-center">
             <h1 class="text-2xl font-bold mb-4">Q{{ questionId }}.</h1>
 
-            <p class="mb-8">질문 내용</p>
+            <p class="mb-8" v-if="currentQuestion">{{ currentQuestion.questionText }}</p>
 
             <div class="flex flex-col space-y-4">
                 <button
+                    v-if="answers.length > 0"
                     v-for="answer in answers"
-                    :key="answer.id"
+                    :key="answer.optionIdx"
                     @click="nextQuestion"
                     class="bg-white text-red-500 font-semibold py-4 px-6 rounded-xl shadow-lg transition duration-300 transform hover:scale-105"
                 >
-                    {{ answer.text }}
+                    {{ answer.optionText }}
                 </button>
             </div>
         </div>
---
+++ src/stores/test.js
@@ -0,0 +1,40 @@
+import { defineStore } from 'pinia';
+import apiInstance from '@/util/axios-instance';
+
+export const useTestStore = defineStore('testStore', {
+  state: () => ({
+      questions: [],
+      options: new Map(),
+  }),
+  actions: {
+      async fetchQuestions() {
+          try {
+              const response = await apiInstance.get('/test/questions'); 
+              if (response.data.success) {
+                  this.questions = response.data.data;
+                  return response.data.data;
+              } else {
+                  console.error('질문을 가져오는 데 실패했습니다.', response.data.error);
+              }
+          } catch (error) {
+            console.error('질문을 가져오는 중 오류가 발생했습니다:', error.message);
+          }
+      },
+      async fetchOptions(questionIdx) {
+        try {
+          const response = await apiInstance.get(`/test/options/${questionIdx}`);
+          if(response.data.success) {
+              this.options.set(questionIdx, response.data.data);
+              return response.data.data;
+          } else {
+              console.error('질문을 가져오는 중 오류가 발생했습니다:', error.message);
+          }
+        } catch (error) {
+            console.error('질문을 가져오는 중 오류가 발생했습니다:', error.message);
+        }
+      },
+      getOptionsByQuestionIdx(questionIdx) {
+        return this.options.get(questionIdx) || [];
+      },
+  },
+});
