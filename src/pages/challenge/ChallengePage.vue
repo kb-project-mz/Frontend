f4db6a223351ab5005fce14b2e544be962904cce
2da00 <86220363+imdayoung@users.noreply.github.com>
2024-10-07 14:46:26 +0900

    [SCRUM-294] Rename: NoMoneyChallenge -> PeerChallenge로 이름 변경

--- src/pages/challenge/ChallengePage.vue
+++ src/pages/challenge/ChallengePage.vue
@@ -1,12 +1,12 @@
 <script setup>
 import MyChallengeList from '@/components/challenge/MyChallengeList.vue';
-import NoMoneyChallenge from '@/components/challenge/NoMoneyChallenge.vue';
+import PeerChallenge from '@/components/challenge/PeerChallenge.vue';
 
 </script>
 
 <template>
   <div class="mx-[10%] grid grid-cols-1 lg:grid-cols-6 gap-20">
-    <NoMoneyChallenge class="lg:col-span-2 max-h-160" />
+    <PeerChallenge class="lg:col-span-2 max-h-160" />
     <MyChallengeList class="lg:col-span-4" />
   </div>
 </template>
