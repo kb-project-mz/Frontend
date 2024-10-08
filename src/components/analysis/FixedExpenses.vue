cab4c8bfcade7953aa6a732e2e777ef7f7ac807b
2da00 <86220363+imdayoung@users.noreply.github.com>
2024-10-08 11:25:37 +0900

    [SCRUM-272] Fix: 고정 지출이 없는 사용자에 대한 예외 처리

--- src/components/analysis/FixedExpenses.vue
+++ src/components/analysis/FixedExpenses.vue
@@ -15,14 +15,19 @@ onMounted(async () => {
 </script>
 
 <template>
-<div class="py-5 px-8 bg-white border border-gray-200 rounded-2xl shadow">
+  <div class="py-5 px-8 bg-white border border-gray-200 rounded-2xl shadow">
     <div class="font-bold mb-3">나의 고정 지출 키워드는?</div>
-    <div class="flex flex-wrap gap-y-2 gap-x-3 mt-2">
-      <div v-for="expense in fixedExpenses" :key="expense" class="py-2 px-3 w-fit rounded-full text-white bg-navy text-xs">
-        #{{ expense }}
+    <div v-if="fixedExpenses.length > 0">
+      <div class="flex flex-wrap gap-y-2 gap-x-3 mt-2">
+        <div v-for="expense in fixedExpenses" :key="expense" class="py-2 px-3 w-fit rounded-full text-white bg-navy text-xs">
+          #{{ expense }}
+        </div>
       </div>
     </div>
-</div>
+    <div v-else>
+      <div class="text-center">고정 지출이 없어요!</div>
+    </div>
+  </div>
 </template>
 
 <style>
