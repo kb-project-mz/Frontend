5887e4134bfc38d6f5c8f954a688dfbf06e406b7
woo427 <annejwkim00@naver.com>
2024-10-09 12:56:44 +0900

    [SCRUM-209] Feat: 소비테스트 결과지 불러오기
    - 소비테스트 결과지 불러오고, 소비테스트를 하지 않은 사용자의 경우 소비테스트하러가도록 라우팅 설정

--- src/components/home/Member.vue
+++ src/components/home/Member.vue
@@ -6,12 +6,14 @@ import challengeImage from '@/assets/챌린지.png';
 
 import Account from '@/components/home/Account.vue';
 import Challenge from '@/components/home/Challenge.vue';
+import SBTIResult from './SBTIResult.vue';
 </script>
 
 <template>
   <div class="mx-[10%] grid grid-cols-1 lg:grid-cols-3 gap-20">
     <div>
       <h1>[소비 테스트 결과지]</h1>
+      <SBTIResult />
     </div>
     <div>
       <h1>[challenge top3]</h1>
---
+++ src/components/home/SBTIResult.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { useTestResultStore } from '@/stores/testResult';
+import { ref, onMounted } from 'vue';
+
+const testResultStore = useTestResultStore();
+const SBTIResult = ref([]);
+
+const authData = JSON.parse(localStorage.getItem('auth'));
+const memberIdx = authData.memberIdx;
+
+const fetchSBTIResult = async (memberIdx) => {
+  await testResultStore.getTestResult(memberIdx);
+};
+
+onMounted(async () => {
+  await fetchSBTIResult(memberIdx);
+  SBTIResult.value = testResultStore.TestResult;
+});
+</script>
+
+<template>
+  <div v-if="memberIdx">
+    {{ SBTIResult.typeImage }}
+  </div>
+  <div v-else>
+    <RouterLink to="/test/start"> 소비 테스트 하러가기! </RouterLink>
+  </div>
+</template>
