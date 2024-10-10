ea7c60f1e89f2b2091c289c3ed0ed60217ed0eb4
JUNG DAYE <jungyj0809@gmail.com>
2024-10-10 23:28:40 +0900

    [SCRUM-323] Feat: 서버에서 불러오는 값에 memberIdx 추가
    - 구글 소셜 로그인 시, 서버에서 프론트로 구글 토큰 불러오는 값에 memberIdx 추가

--- src/pages/login/GoogleCallBack.vue
+++ src/pages/login/GoogleCallBack.vue
@@ -11,24 +11,39 @@ const route = useRoute();
 const googleAccessToken = computed(() => route.query.access_token);
 const googleRefreshToken = computed(() => route.query.refresh_token);
 const memberId = computed(() => route.query.member_id);
+const memberIdx = computed(() => route.query.member_idx);
 const memberName = computed(() => route.query.member_name);
 
 onMounted(() => {
+
+  console.log("초기 구글 토큰 값 확인:", googleAccessToken.value, googleRefreshToken.value);
+  console.log("초기 memberId 및 memberIdx 값 확인:", memberId.value, memberIdx.value);
+
   if (googleAccessToken.value) {
     try {
       authStore.$patch({
         member: {
           memberId: memberId.value,
           memberName: memberName.value,
-        }
+          memberIdx: memberIdx.value,
+        },
+        accessToken: googleAccessToken.value || localStorage.getItem('accessToken')
       });
+
+      console.log("AuthStore에 저장된 데이터:", authStore.member);
+      console.log("AuthStore의 accessToken 확인:", authStore.accessToken);
+      console.log("AuthStore의 로그인 상태 확인:", authStore.isLogin());
 
       setLocalStorage({
         memberId: memberId.value,
         accessToken: googleAccessToken.value,
         refreshToken: googleRefreshToken.value,
-        memberName: memberName.value
+        memberName: memberName.value,
+        memberIdx: memberIdx.value,
       });
+
+      console.log("AuthStore에 저장된 데이터:", authStore.member);
+      console.log("로컬 스토리지에 저장된 auth:", localStorage.getItem('auth'));
 
       authStore.loadAuthState();
       router.push('/'); 
@@ -38,3 +53,4 @@ onMounted(() => {
   }
 });
 </script>
+
