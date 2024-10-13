27a2ba02d4d409242183163bb2263df1940309f6
다영 <86220363+imdayoung@users.noreply.github.com>
2024-10-13 17:57:08 +0900

    [SCRUM-255] Design: 등록된 챌린지가 없는 경우 예외 처리

--- src/components/challenge/MyChallengeList.vue
+++ src/components/challenge/MyChallengeList.vue
@@ -32,7 +32,7 @@ onMounted(async () => {
 </script>
 
 <template>
-  <div>
+  <div v-if="challengeStore.challengeList.length > 0">
     <div class="flex items-center justify-between mb-4">
       <div class="text-xl font-bold text-center flex-grow">
         나의 챌린지 목록
@@ -57,6 +57,15 @@ onMounted(async () => {
       @close="closeCreateChallengeModal"
       @challengeAdded="addNewChallenge"
     />
+  </div>
+  <div v-else>
+    <div class="flex flex-col items-center py-60">
+      <div class="text-9xl font-black mt-6 mb-8 text-gray-600">텅</div>
+      <div class="text-xl text-center">
+        <div>등록된 챌린지가 없어요</div>
+        <div>챌린지를 등록하고 안정적인 소비 습관을 만들어봐요!</div>
+      </div>
+    </div>
   </div>
 </template>
 
